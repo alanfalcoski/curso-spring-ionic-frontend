aa5baaf797f5b1fce7151bdf471aeafcb43e9cec
Alan Rodrigues <falcoskialan@gmail.com>
2019-12-18 23:18:47 -0300

    Mostrando as categorias

--- src/config/api.config.ts
+++ src/config/api.config.ts
@@ -1,3 +1,5 @@
 export const API_CONFIG = {
-  base_url: "http://localhost:8080"
+  base_url: "http://localhost:8080",
+  bucket_base_url:
+    "https://curso-spring-ionic-afalcoski.s3-sa-east-1.amazonaws.com"
 };
